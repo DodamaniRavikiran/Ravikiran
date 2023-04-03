d7505d4b30f4f0255f72cd88447b05fe1401294a
DodamaniRavikiran <129724805+DodamaniRavikiran@users.noreply.github.com>
2023-04-03 12:25:35 +0530

    Add files via upload

---
+++ Assignmentoperators.java
@@ -0,0 +1,12 @@
+class Assignmentoperators
+{
+public static void main (String[]args)
+{
+int a = 10;
+System.out.println(a+=5);
+System.out.println(a-3);
+System.out.println(a*=4);
+System.out.println(a/=3);
+System.out.println(a%=4);
+}
+}
